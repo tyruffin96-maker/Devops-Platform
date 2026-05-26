a8958dc5e41ff813061424d86c1dd2f58ad1491c
tyruffin96-maker <tyruffin96@gmail.com>
2026-05-26 13:04:09 -0400

    fix: add instance_id output to ec2 module, remove duplicate monitoring block from vpc

--- modules/vpc/ec2/outputs.tf
+++ modules/vpc/ec2/outputs.tf
@@ -2,3 +2,6 @@ output "bastion_public_ip"  { value = aws_instance.bastion.public_ip }
 output "app_private_ip"     { value = aws_instance.app.private_ip }
 output "jenkins_private_ip" { value = aws_instance.jenkins.private_ip }
 output "deployer_key_name"  { value = aws_key_pair.deployer.key_name }
+output "instance_id" {
+  value = aws_instance.app.id
+}
